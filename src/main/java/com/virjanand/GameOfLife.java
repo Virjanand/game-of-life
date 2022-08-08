dce8f16fa362adaa5e4ed2857f4ae630f7d0271a
Virjanand Panday <virjanand@virjanand.com>
2022-08-08 18:47:13 +0200

    Green test: print starting position

--- src/main/java/com/virjanand/GameOfLife.java
+++ src/main/java/com/virjanand/GameOfLife.java
@@ -2,6 +2,10 @@ package com.virjanand;
 
 public class GameOfLife {
     public void printPosition() {
-
+        System.out.print("" +
+                "........\n" +
+                "....*...\n" +
+                "...**...\n" +
+                "........");
     }
 }
